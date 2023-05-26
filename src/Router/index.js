c32c40ac9257c72c09369c8fd5d188e0d7c3cf7f
MNSM92 <mnsm.pbl@gmail.com>
2023-05-26 14:39:25 -0400

    initial 1.1

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from '../Components/index';
-import WebView from '../Components/WebView';
-import Frame1 from '../Components/Frame1';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
+import WebView from '../Components/WebView'; 
+
+
 const RouterDOM = () => {
 	return (
 		<Router>
 			<Switch>
-				<Route exact path="/"><HomePage /></Route>
-				<Route exact path="/webview"><WebView /></Route>
-				<Route exact path="/frame1"><Frame1 /></Route>
+				<Route exact path="/"><WebView /></Route>
+				
 			</Switch>
 		</Router>
 	);
